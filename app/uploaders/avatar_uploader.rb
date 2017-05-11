5bf0d76b97ffe325f6e5eeff90fa353ecee1e6df
Scott Robbin <scott@robbin.co>
2017-05-11 14:48:40 -0500

    Respect photo orientation.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,4 @@
-class AvatarUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-
+class AvatarUploader < BaseUploader
   storage :fog
 
   version :thumb do
@@ -28,19 +26,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
     end
   end
 
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url(*_args)
     ActionController::Base.helpers.asset_path "default_avatar.png"
-  end
-
-  def fog_public
-    false
   end
 end
