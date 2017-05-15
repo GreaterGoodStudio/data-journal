740d083cd2207b8c944fbce15338bc4a3f5572fc
Scott Robbin <scott@robbin.co>
2017-05-15 12:04:34 -0500

    Create data point from photo.

--- app/policies/photo_policy.rb
+++ app/policies/photo_policy.rb
@@ -2,4 +2,8 @@ class PhotoPolicy < ApplicationPolicy
   def destroy?
     record.member == user && record.photographable_type == "Session"
   end
+
+  def create_data_point?
+    destroy?
+  end
 end
