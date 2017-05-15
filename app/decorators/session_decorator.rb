6eb85f452a225c265f16b19a1fe80ff1cd15f821
Scott Robbin <scott@robbin.co>
2017-05-15 10:39:05 -0500

    Session thumbnails.

--- app/decorators/session_decorator.rb
+++ app/decorators/session_decorator.rb
@@ -1,3 +1,17 @@
 class SessionDecorator < BaseDecorator
   decorates_association :member
+
+  def thumbnail
+    image_tag thumbnail_image, class: "ui image"
+  end
+
+  def formatted_date
+    object.date.strftime("%m/%d/%Y")
+  end
+
+  private
+
+    def thumbnail_image
+      object.photos.any? ? object.photos.last.image_url(:thumb) : asset_url("default_photo.png")
+    end
 end
